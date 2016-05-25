83e46ffbdb98b9260d41445bfb8b9c4e08f4bf64
papenbergmichael <papenbergmichael@gmail.com>
2016-05-25 13:41:54 +0200

    Kantenausgabe für Trees hinzugefügt

--- AlogDatPraktikum/AlogDatPraktikum/Tree/BaseTree.cs
+++ AlogDatPraktikum/AlogDatPraktikum/Tree/BaseTree.cs
@@ -18,19 +18,24 @@ namespace AlogDatPraktikum
                 Console.WriteLine("Baum ist leer.");
         }
 
-        protected void _Print_(TreeNode node, int level = 0)
+        protected void _Print_(TreeNode node, int level = 0, Direction dir = Direction.undef)
         {
             //Teilbaum rechts ausgeben
             if (node.right != null)
-                _Print_(node.right, level + 1);
+                _Print_(node.right, level + 1, Direction.right);
             //element ausgeben
-            string output = new String(' ', 8 * level) + node.elem.elemValue;
+            string output = new String(' ', 5 * level);
+            if (dir == Direction.left)
+                output += "\\";
+            else if (dir == Direction.right)
+                output += "/";
+            output += node.elem.elemValue;
             if (node.GetType() == typeof(AVLTreeNode))
                 output += "[" + (node as AVLTreeNode).BalanceFaktor + "]";
             Console.WriteLine(output);
             //Teilbaum links ausgeben
             if (node.left != null)
-                _Print_(node.left, level + 1);
+                _Print_(node.left, level + 1, Direction.left);
 
         }
     }
