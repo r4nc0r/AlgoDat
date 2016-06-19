a18d6933123bd44649a6c2b64c9112fe14f04947
Tobias <Tobias@TOBIAS-PC>
2016-06-19 15:54:54 +0200

    Revert "Kommmentare, Ausgabe der HashtabSepChain"
    This reverts commit 38f107cd53b39da9db7d22a8e0948eac467722e0.

--- AlogDatPraktikum/AlogDatPraktikum/Hash/HashTabQuadProb.cs
+++ AlogDatPraktikum/AlogDatPraktikum/Hash/HashTabQuadProb.cs
@@ -10,22 +10,18 @@ namespace AlogDatPraktikum
     {
         DictElement[] hashTab;
         int TabLength, freespace;
-
-        /// <summary>
-        /// Konstruktor
-        /// </summary>
         public HashTabQuadProb()
         {
             Console.WriteLine("wie groß soll die Hashtabelle mindestens sein?");
             Console.Write("Bitte geben Sie eine Primzahl ein ");
             int inp = Convert.ToInt32(Console.ReadLine());
+
+
             TabLength = calcNextPrim(inp);
             hashTab = new DictElement[TabLength];
             Console.WriteLine(TabLength);
             initHashTab();
         }
-
-
         /// <summary>
         /// initiailisiert alle Einträge der Hashtabelle auf -1
         /// </summary>
@@ -44,32 +40,23 @@ namespace AlogDatPraktikum
                 return false;
 
             hashTab[pos].elemValue = -1;
-            return true;
+                return true;
+            
+            
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="elem"></param>
-        /// <returns></returns>
         public override bool Insert(int elem)
         {
             for (int i = 0; i < TabLength; i++)
             {
-                //Berechnung der Pos an welcher eingefügt werden soll
                 int pos = calcHash(elem, i, TabLength);
-                // wenn die Position kleiner 0 ist (kann nur -1 sein) ist keine freie Stelle mehr frei 
-                // welche sondiert werden kann
                 if(pos < 0)
                 {
-                   
+                    Console.WriteLine("Kein Speicherplatz vorhanden");
                     return false;
                 }
-                // wenn sich an der sondierten Stelle ein Element mit dem selben Value befindet muss nicht neu eingefügt werden
                 if (hashTab[pos].elemValue == elem)
                 { return false; }
-                
-                //befindet sich an der sondierten Position noch kein Element kann hier eingefügt werden
                 if (hashTab[pos].elemValue == -1)
                 {
                     freespace--;
@@ -93,12 +80,10 @@ namespace AlogDatPraktikum
         public bool Search(int elem)
         {
             int pos = privSearch(elem);
-            
             if (pos!=-1 && hashTab[pos].elemValue != -1)
                 return true;
             return false;
         }
-
 
         private int privSearch(int elem)
         {
@@ -116,12 +101,6 @@ namespace AlogDatPraktikum
             return -1;
         }
 
-        /// <summary>
-        /// Überschreibt die Methode calcNextPrim aus der Base Klasse
-        /// in der Art dass hier eine Primzahl der Form 4k+3 gesucht wird
-        /// </summary>
-        /// <param name="Input"></param>
-        /// <returns></returns>
         public override int calcNextPrim(int Input)
         {
 
@@ -130,6 +109,7 @@ namespace AlogDatPraktikum
             {
                 Input += 2;
                 Input = base.calcNextPrim(Input);
+
             } 
 
             return Input;
@@ -139,9 +119,8 @@ namespace AlogDatPraktikum
 
         private int calcHash(int Element, int j, int c)
         {
-            // h(i,j)= h(i)+(-1)^j+1 *i²/2 Funktion zum quadratischen sondieren
-            //j zählt mit jeder Iteration nach oben und alterniert dabei das Vorzeichen  j² & -j² 
-            int mult = (int)(Math.Ceiling(j  / 2.0)); 
+
+            int mult = (int)(Math.Ceiling(j  / 2.0));
             mult = mult * mult;
             int temp = hashfuntion(Element, c) + (int)(Math.Pow(-1, (j + 1)) * mult);
 
--- AlogDatPraktikum/AlogDatPraktikum/Hash/HashTabSepChain.cs
+++ AlogDatPraktikum/AlogDatPraktikum/Hash/HashTabSepChain.cs
@@ -11,9 +11,9 @@ namespace AlogDatPraktikum
 
         SetUnsortedLinkedList[] hashChain;
         private int TabLength;
-        
         /// <summary>
-        /// Konstruktor fragt naach anzulegender Tabellengröße und prüft ob es sich um eine Primzahl handelt.
+        /// Problem noch mit basisklassen konstruktor, es wird immer ein basehash tabelle erstellt
+        /// hab noch keine idee das zu umgehen
         /// </summary>
         public HashTabSepChain()
         {
@@ -30,8 +30,6 @@ namespace AlogDatPraktikum
             int hashvalue = base.hashfuntion(elem, TabLength);
             if (hashChain[hashvalue] != null)
             {
-                //sucht an der Stelle wo der Wert stehen sollte und löscht diesen falls vorhanden, 
-                //nutzt dazu die Methoden der SetUnsortedLinkedList
                 bool flag= hashChain[hashvalue].Delete(elem);
                 //Falls Liste nach dem löschen eines Elements leer, dann Eintrag in HashChain auf Null setzten
                 if (hashChain[hashvalue].Root == null)
@@ -41,14 +39,6 @@ namespace AlogDatPraktikum
                 return false;
         }
 
-        /// <summary>
-        /// 1.Hashwert anhand des Keys berechene
-        /// 2.  und in Array einfügen, wenn an der Stelle schon ein Wert steht
-        ///     wird eine Liste SetUnsortedLinkedList an diesem Platz erstellt und der einzufügende Wert
-        ///     am Ende der jeweiligen Liste angehängt
-        /// </summary>
-        /// <param name="elem"></param>
-        /// <returns></returns>
         public override bool Insert(int elem)
         {
             int hashValue = base.hashfuntion(elem,TabLength);
@@ -62,24 +52,16 @@ namespace AlogDatPraktikum
 
         public override void Print()
         {
-            for (int i = 0; i < hashChain.Length; i++)
+            foreach (SetUnsortedLinkedList item in hashChain)
             {
-                if (hashChain[i] != null)
+                if (item != null)
                 {
-                    Console.Write("Key: {0} Values: ",i);
-                    hashChain[i].Print();
+                    item.Print();
                     Console.WriteLine();
                 }
             }
         }
 
-        /// <summary>
-        /// berechent den Hashwert, und sucht an dieser Stelle
-        /// sollten mehrere Werte unter dem gleichen Key abgelgt worden sein 
-        /// wird die Liste für diesen Key durchsucht
-        /// </summary>
-        /// <param name="elem"></param>
-        /// <returns></returns>
         public bool Search(int elem)
         {
             int hashvalue = base.hashfuntion(elem,TabLength);
